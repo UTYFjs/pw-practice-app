313a2852cb3250180aad16081a0bb1e9a39f905c
UTYFjs <utyf-90@mail.ru>
2024-07-12 11:33:16 +0300

    feat: add Page Object Mpdel  - Navigation Page

---
+++ page-object/navigationPage.ts
@@ -0,0 +1,41 @@
+import { Locator, Page } from '@playwright/test';
+
+export class NavigationPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+  async formLayoutsPage() {
+    await this.selectGroupMenuItem("Forms");
+    await this.page.getByText("Form Layouts").click();
+  }
+
+  async datepickerPage() {
+    await this.selectGroupMenuItem("Forms");
+    await this.page.waitForTimeout(1000);
+    await this.page.getByText("Datepicker").click();
+  }
+
+  async toastrPage() {
+    await this.selectGroupMenuItem("Modal & Overlays");
+    await this.page.waitForTimeout(1000);
+    await this.page.getByText("Toastr").click();
+  }
+  async tooltipPage() {
+    await this.selectGroupMenuItem("Modal & Overlays");
+    await this.page.waitForTimeout(1000);
+    await this.page.getByText("Tooltip").click();
+  }
+  async smartTablePage() {
+    await this.selectGroupMenuItem("Tables & Data");
+    await this.page.waitForTimeout(1000);
+    await this.page.getByText("Smart Table").click();
+  }
+
+  private async selectGroupMenuItem(groupItemTitle: string) {
+    const groupMenuItem = this.page.getByTitle(groupItemTitle);
+    const expandedState = await groupMenuItem.getAttribute("aria-expanded");
+    if (expandedState === "false") await groupMenuItem.click();
+  }
+}
